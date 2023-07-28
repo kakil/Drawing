b5d3fff51c419d8a536f73235b0639839fc1d0b8
Kitwana Akil <kakil@me.com>
2023-07-28 07:54:17 -0400

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Kitwana Akil on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
